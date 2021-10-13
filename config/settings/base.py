4024d02877ef36a76a5e6e8f76d9639c1865aa77
Freston5 <epoealan@gmail.com>
2021-10-13 15:54:06 -0300

    Pagina_Web_Dfconfecciones_2021_Django_V_01

--- config/settings/base.py
+++ config/settings/base.py
@@ -151,11 +151,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join('static'),
 )
 
 
