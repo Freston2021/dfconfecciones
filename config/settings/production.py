ed7ca39d9dd19cba02a1ade86d8b2ba86f71ae1f
Freston5 <epoealan@gmail.com>
2021-10-14 18:06:50 -0300

    PaginaWeb_Dfconfecciones_2021 V.4

--- config/settings/production.py
+++ config/settings/production.py
@@ -100,7 +100,7 @@ DATABASES = {
 
 
 #CACHING------------------------------------------------------------------------
-#REDIS_LOCATION = '{0}/{1}'.format(env('REDIS_URL', default='redis://127.0.0.1:6379'), 0)
+REDIS_LOCATION = '{0}/{1}'.format(env('REDIS_URL', default='redis://127.0.0.1:6379'), 0)
 
 CACHES = {
     'default': {
