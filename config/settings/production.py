4b691e81b5883106adcdae463d5d7792fa63d7c1
Freston5 <epoealan@gmail.com>
2021-10-19 11:11:52 -0300

    actualización a Django 3.2

--- config/settings/production.py
+++ config/settings/production.py
@@ -28,8 +28,8 @@ SECURE_REFERRER_POLICY = 'strict-origin'
 
 
 #STORAGE CONFIGURATION----------------------------------------------------------
-#INSTALLED_APPS += ['gunicorn', 'storages', ]
-INSTALLED_APPS += ['gunicorn',]
+INSTALLED_APPS += ['gunicorn', 'storages', ]
+#INSTALLED_APPS += ['gunicorn',]
 
 AWS_ACCESS_KEY_ID = env("DJANGO_AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("DJANGO_AWS_SECRET_ACCESS_KEY")
@@ -45,8 +45,8 @@ aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws
 
 MEDIA_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
 
-#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #EMAIL--------------------------------------------------------------------------
 DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL',
                          default='PAGINAWEB <paginaweb@daysifernandez.com>')
