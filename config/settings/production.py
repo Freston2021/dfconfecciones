ab449d3e489aa9ab2de4930ba4f44ef5636dd2f1
Freston5 <epoealan@gmail.com>
2021-10-19 13:17:03 -0300

    actualización a Django 3.2

--- config/settings/production.py
+++ config/settings/production.py
@@ -42,9 +42,10 @@ AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
 MEDIA_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 #EMAIL--------------------------------------------------------------------------
 DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL',
