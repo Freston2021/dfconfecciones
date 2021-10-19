3fccf5039a6febde3e7e91dd90b3c79f64094af3
Freston5 <epoealan@gmail.com>
2021-10-19 18:02:23 -0300

    actualización a Django 3.2

--- config/settings/base.py
+++ config/settings/base.py
@@ -26,6 +26,7 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'django.contrib.admin',
 ]
@@ -67,6 +68,7 @@ CORS_ALLOWED_ORIGINS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -174,7 +176,10 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #print ("Static Root path -->", STATIC_ROOT)
 
-STATIC_URL = '/static/'
+#whitenoise#####################################################################
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_HOST = os.environ.get('DJANGO_STATIC_HOST', '')
+STATIC_URL = STATIC_HOST + '/static/'
 #print ("Static Url path -->", STATIC_URL)
 
 STATICFILES_DIRS = (
