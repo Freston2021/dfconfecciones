ab6b18d564e7a13e8a405237add09a6c82370a58
Freston5 <epoealan@gmail.com>
2021-10-21 17:23:58 -0300

    Post_ContactForm_Photologue_Newsletter-->OK in Django 3.2

--- config/settings/production.py
+++ config/settings/production.py
@@ -46,21 +46,21 @@ MEDIA_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 #EMAIL--------------------------------------------------------------------------
-DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL',
-                         default='PAGINAWEB <paginaweb@daysifernandez.com>')
+#DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL',
+#                         default='PAGINAWEB <paginaweb@daysifernandez.com>')
 
-EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[daysiweb]')
+#EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[daysiweb]')
 
-SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
+#SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
 
 # Anymail with Mailgun
-INSTALLED_APPS += ['anymail', ]
+#INSTALLED_APPS += ['anymail', ]
 
-ANYMAIL = {
-    'MAILGUN_API_KEY': 'key-48650e6634bd972b621fae537f39cafa',
-    'MAILGUN_SENDER_DOMAIN': 'sandboxd3d8ace8e76c47dcb7a7507df5c56455.mailgun.org'
-}
-EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
+#ANYMAIL = {
+#    'MAILGUN_API_KEY': 'key-48650e6634bd972b621fae537f39cafa',
+#    'MAILGUN_SENDER_DOMAIN': 'sandboxd3d8ace8e76c47dcb7a7507df5c56455.mailgun.org'
+#}
+#EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
 
 #TEMPLATE CONFIGURATION---------------------------------------------------------
 TEMPLATES[0]['OPTIONS']['loaders'] = [
